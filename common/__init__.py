27e45fd0914d2884b7257a8850a6c982fd43fc1f
Deeksha Vishnoi <deekshavishnoi7@gmail.com>
2025-11-25 17:05:32 +0100

    Create __init__.py

---
+++ common/__init__.py
@@ -0,0 +1,11 @@
+"""
+common package
+
+Shared utility functions used across all BMW challenge tasks:
+- io_utils.py: file I/O, logging, config handling
+- viz.py: lightweight plotting and visualization helpers
+
+This makes sure we don’t repeat the same helper code in every task.
+"""
+
+__version__ = "0.1.0"
